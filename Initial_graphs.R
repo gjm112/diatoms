b36655dc710a5bc29c0189697760913af5b09b82
Sydney B <sydneyb@Sydneys-MacBook-Pro-2.local>
2026-04-07 15:58:30 -0500

    bro

--- Initial_graphs.R
+++ Initial_graphs.R
@@ -1,6 +1,3 @@
-
-
-
 library(dplyr)
 
 summary_data <- finallong %>%
@@ -23,9 +20,13 @@ ggplot(summary_data, aes(x = Mean.time.of.deposition, y = count, color = as.fact
 
 #completely messed up this graph, but it does show that we do not have the same count of diatoms throughout the different timepoints
 #ranges from about 1225 to 1455. Is this a problem
+test2 <- finallong %>% group_by(D_level,sample_num, Mean.time.of.deposition, Depth..m.,Duration,Elements,) %>% summarize(Count = sum(Count))
 
+unique <- unique(test2$Elements)
 
-combined_data <- test %>%
+taxon <- taxon %>% filter(species %in% unique)
+
+combined_data <- test2 %>%
   left_join(taxon, by = c("Elements" = "species"))
 
 
@@ -35,17 +36,19 @@ combined_data <- combined_data %>%
   mutate(D_level = factor(D_level, levels = d_order))
 
 plot_data <- combined_data %>%
-  group_by(major_group, D_level) %>%
-  summarise(total_val = sum(Count, na.rm = TRUE), .groups = "drop") 
-
-ggplot(plot_data, aes(x = D_level, y = total_val, color = major_group, group = major_group)) +
+  group_by(major_group, Mean.time.of.deposition) %>%
+  summarise(total_val = sum(Count, na.rm = T), .groups = "drop") 
+#facet wrap with free scales
+ggplot(plot_data %>% filter(!is.na(Mean.time.of.deposition)), aes(x = Mean.time.of.deposition, y = total_val, color = major_group, group = major_group)) +
   geom_line(linewidth = 1) +
   geom_point() +
   labs(
-    title = "Total Counts by Major Group across D-Levels",
-    x = "D-Level",
+    title = "Total Counts by Major Group across time points",
+    x = "Mean time of deposition",
     y = "Sum of Counts",
     color = "Major Group"
   ) +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_wrap(~major_group, scales="free") +
+  geom_smooth()
