6bb1b8a4c80bb8b7234a5ba047d9090e342bedb4
Sydney B <sydneyb@Sydneys-MacBook-Pro-2.local>
2026-04-20 09:46:41 -0500

    ready for Yoel?

--- Initial_graphs.R
+++ Initial_graphs.R
@@ -1,25 +1,14 @@
 library(dplyr)
+library(tidyverse)
+library(ggplot2)
 
 summary_data <- finallong %>%
   group_by(Mean.time.of.deposition, D_level) %>%
   summarise(count = sum(Count), .groups = "drop")
 
 
-library(ggplot2)
-
-ggplot(summary_data, aes(x = Mean.time.of.deposition, y = count, color = as.factor(D_level))) +
-  geom_line() +
-  geom_point() +
-  labs(
-    x = "Mean Time",
-    y = "Count",
-    color = "D-Level"
-  ) +
-  theme_minimal()
 
 
-#completely messed up this graph, but it does show that we do not have the same count of diatoms throughout the different timepoints
-#ranges from about 1225 to 1455. Is this a problem
 test2 <- finallong %>% group_by(D_level,sample_num, Mean.time.of.deposition, Depth..m.,Duration,Elements,) %>% summarize(Count = sum(Count))
 
 unique <- unique(test2$Elements)
@@ -36,19 +25,29 @@ combined_data <- combined_data %>%
   mutate(D_level = factor(D_level, levels = d_order))
 
 plot_data <- combined_data %>%
+  filter(major_group != "Unidentified") %>%
   group_by(major_group, Mean.time.of.deposition) %>%
   summarise(total_val = sum(Count, na.rm = T), .groups = "drop") 
-#facet wrap with free scales
-ggplot(plot_data %>% filter(!is.na(Mean.time.of.deposition)), aes(x = Mean.time.of.deposition, y = total_val, color = major_group, group = major_group)) +
+
+
+ggplot(plot_data %>% filter(!is.na(Mean.time.of.deposition)), aes(x = Mean.time.of.deposition, y = total_val,  group = major_group)) +
   geom_line(linewidth = 1) +
   geom_point() +
   labs(
     title = "Total Counts by Major Group across time points",
     x = "Mean time of deposition",
     y = "Sum of Counts",
-    color = "Major Group"
   ) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   facet_wrap(~major_group, scales="free") +
   geom_smooth()
+
+#The graph above shows the counts for the different major groups through time.
+#It is important to note that the scales on the Y-axis are different for the different groups.
+#The groups that seem to be the most prevelant are Centric, NDO, and Pennate.
+
+
+#hypothesis test for trend between time and count
+#time point pairs
+#deviation form constant mean
